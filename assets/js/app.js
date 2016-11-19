2fd39c1ec6db58f196c4f71babed01894a464b9c
Daniël Ducro <info@danield.nl>
2016-11-19 17:52:34 +0100

    Update 2016-11-19T16:52:32.635Z

--- assets/js/app.js
+++ assets/js/app.js
@@ -302,3 +302,107 @@ Board.prototype = {
         return 'color-' + this.colors[colorIndex] + '-' + this.paters[paterIndex];
     }
 };
+var Cell = function(point) {
+    this.point = point;
+    this.$el = null;
+    this.createElement();
+}
+
+Cell.prototype = {
+    createElement: function() {
+        this.$el = $('<div class="cell"></div>');
+        this.$el.data('cell', this);
+    },
+    fill: function(color) {
+        this.removeColor();
+        this.$el.addClass(color);
+    },
+    removeColor: function() {
+        this.$el.removeClass(function(index, css) {
+            return (css.match(/(^|\s)color-\S+/g) || []).join(' ');
+        });
+    }
+};
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+$(document).ready(function() {
+    var board = new Board();
+    board.init();
+});
+var Point = function(x, y) {
+    this.x = x;
+    this.y = y;
+}
+var Shape = function(start, end, color) {
+    this.start = start;
+    this.end = end;
+    this.id = null;
+    this.color = color;
+    this.cells = [];
+    this.value = 0;
+    this.topLeft = null;
+    this.bottomRight = null;
+
+    this.setId();
+    this.setTopLeftBottomRight();
+}
+
+Shape.prototype = {
+    setId: function() {
+        var w = Math.abs(this.start.x - this.end.x);
+        var h = Math.abs(this.start.y - this.end.y);
+        this.id = w + 'x' + h;
+        this.value = (w + 1) * (h + 1);
+    },
+    setEnd: function(end) {
+        this.end = end;
+        this.setId();
+        this.setTopLeftBottomRight();
+    },
+    setTopLeftBottomRight: function() {
+        this.topLeft = jQuery.extend({}, this.start);
+        this.bottomRight = jQuery.extend({}, this.end);
+
+        if (this.end.x < this.start.x) {
+            this.topLeft.x = this.end.x;
+            this.bottomRight.x = this.start.x;
+        }
+        if (this.end.y < this.start.y) {
+            this.topLeft.y = this.end.y;
+            this.bottomRight.y = this.start.y;
+        }
+    },
+    has: function(cell) {
+        if (cell.point.x >= this.topLeft.x && cell.point.x <= this.bottomRight.x
+            && cell.point.y >= this.topLeft.y && cell.point.y <= this.bottomRight.y) {
+            return true;
+        }
+
+        return false;
+    },
+    clearCells: function() {
+        this.cells = [];
+    },
+    addCell: function(cell) {
+        this.cells.push(cell);
+    },
+    fill: function() {
+        var self = this;
+        this.cells.forEach(function(cell) {
+            cell.fill(self.color);
+        });
+    },
+    removeFill: function() {
+        this.cells.forEach(function(cell) {
+            cell.removeColor();
+        });
+    }
+};
--- resources/js/main.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-    var board = new Board();
-    board.init();
-});
