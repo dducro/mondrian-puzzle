7bacbc45ed1e15d990605d65015201e37e3a843f
Daniël Ducro <info@danield.nl>
2016-12-01 22:42:49 +0100

    Update 2016-12-01T21:42:47.960Z

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
 var Board = function() {
     this.$board = $('#board');
-    this.$score = $('#score');
     this.$restart = $('#restart');
     this.$reduce = $('#reduce');
     this.$expand = $('#expand');
     this.$size = $('#size');
+    this.$scores = $('#high-scores ul');
 
     this.cells = [];
     this.shapes = [];
@@ -12,18 +12,31 @@ var Board = function() {
     this.shapeToRemove = null;
     this.score = 0;
     this.finished = false;
-    this.size = 10;
-    this.minSize = 4;
-    this.maxSize = 32;
+    this.size = 8;
+    this.minSize = 5;
+    this.maxSize = 25;
+    this.minScore = 0;
+    this.maxScore = 0;
 
     this.colors = ['orange', 'cyan', 'green', 'yellow', 'red', 'indigo', 'brown', 'purple', 'blue-grey', 'grey', 'blue', 'pink'];
-    this.patterns = ['line1', 'line2', 'line3', 'line4', 'zig-zag'];
+    this.patterns = ['vertical', 'horizontal'];
     this.shapeColors = [];
     this.setColors();
+
+    this.scores = {
+        5: 4, 6: 5, 7: 5, 8: 6, 9: 6, 10: 8, 11: 6, 12: 7, 13: 8, 14: 9, 15: 9,
+        16: 9, 17: 8, 18: 10, 19: 9, 20: 9, 21: 9, 22: 9, 23: 9, 24: 9, 25: 11
+    };
+    this.userScores = {
+        5: [], 6: [], 7: [], 8: [], 9: [], 10: [], 11: [], 12: [], 13: [], 14: [], 15: [],
+        16: [], 17: [], 18: [], 19: [], 20: [], 21: [], 22: [], 23: [], 24: [], 25: []
+    }
+    this.currentScores = [];
 }
 
 Board.prototype = {
     init: function() {
+        this.fetchUserScores();
         this.generateBoard();
         this.binds();
         return this;
@@ -39,6 +52,10 @@ Board.prototype = {
 
         this.cells = [];
         this.shapes = [];
+        this.currentScores = this.userScores[this.size];
+        this.setMinMacScores();
+        this.updateScores();
+
         this.$board.empty();
         for (var y = this.size; y > 0; y--) {
             for (var x = 1; x <= this.size; x++) {
@@ -47,7 +64,12 @@ Board.prototype = {
                 this.$board.append(cell.$el);
             }
         }
+
         this.$size.html(this.size + ' x ' + this.size);
+    },
+    setMinMacScores: function() {
+        this.minScore = (this.size * (this.size - 1)) - 1;
+        this.maxScore = this.scores[this.size];
     },
     binds: function() {
         this.$board.on('click', '.cell', $.proxy(this.onClick, this));
@@ -58,6 +80,15 @@ Board.prototype = {
         this.$restart.on('click', $.proxy(this.onRestart, this));
         this.$reduce.on('click', $.proxy(this.onReduceSize, this));
         this.$expand.on('click', $.proxy(this.onExpandSize, this));
+    },
+    fetchUserScores: function() {
+        var userScores = JSON.parse(localStorage.getItem('userScores'));
+        if (userScores != null && typeof userScores === 'object') {
+            this.userScores = userScores;
+        }
+    },
+    storeUserScores: function() {
+        localStorage.setItem('userScores', JSON.stringify(this.userScores));
     },
     draw: function() {
         this.shapes.forEach(function(shape) {
@@ -74,7 +105,6 @@ Board.prototype = {
         if (this.validate(newShape)) {
             this.addShape(newShape);
             this.shapes.push(newShape);
-            this.calculateScore();
             this.checkFinished();
         } else {
             this.removeShapeCells(newShape);
@@ -98,7 +128,6 @@ Board.prototype = {
         }
         this.removeShapeCells(removeShape);
         this.reuseColor(removeShape.color);
-        this.calculateScore();
 
         this.shapes = this.shapes.filter(function(shape) {
             return removeShape.id != shape.id;
@@ -143,12 +172,12 @@ Board.prototype = {
         return overlap;
     },
     shake: function() {
-        this.animation(this.$board, 'shake', 800);
+        this.animate(this.$board, 'shake', 800);
     },
     error: function($el) {
-        this.animation($el, 'error', 3000);
+        this.animate($el, 'error', 3000);
     },
-    animation: function($el, animation, duration) {
+    animate: function($el, animation, duration) {
         $el.addClass(animation);
         setTimeout(function() {
             $el.removeClass(animation);
@@ -158,6 +187,7 @@ Board.prototype = {
         if (this.currentShape === null) {
             this.currentShape = this.createShape(cell.point, cell.point);
             this.addShape(this.currentShape);
+            this.moveCursor();
         }
     },
     removeCurrentShape: function() {
@@ -174,6 +204,7 @@ Board.prototype = {
     checkFinished: function() {
         if (this.availableCells().length == 0) {
             this.finished = true;
+            this.calculateScore();
         }
     },
     calculateScore: function() {
@@ -184,13 +215,45 @@ Board.prototype = {
 
             var low = sortedShapes[0].value;
             var high = sortedShapes[sortedShapes.length - 1].value;
-            this.score = high - low;
-        }
-        else {
-            this.score = 0;
-        }
+            var score = high - low;
 
-        this.$score.html(this.score);
+            if (score >= this.maxScore && score <= this.minScore) {
+                this.score = score;
+
+                this.currentScores.push(this.score);
+                this.currentScores.sort(this.sortNumber);
+                if (this.currentScores.length > 8) {
+                    this.currentScores.pop();
+                }
+                this.userScores[this.size] = this.currentScores;
+                this.storeUserScores();
+                this.updateScores();
+                this.animateScore();
+            }
+        }
+    },
+    updateScores: function() {
+        var self = this;
+        this.$scores.empty();
+        this.currentScores.forEach(function(score) {
+            var $el = $('<li></li>'),
+                percentage = self.calculatePercentage(score);
+
+            $el.addClass('score_' + score);
+            $el.text(score + ' (' + percentage + '%)');
+            self.$scores.append($el);
+        });
+    },
+    calculatePercentage: function(score) {
+        var diffScore = this.minScore - this.maxScore;
+        var percentageScore = this.minScore - score;
+        var percentage = percentageScore / diffScore * 100;
+
+        return Math.round(percentage);
+    },
+    animateScore: function() {
+        var $score = this.$scores.find('.score_' + this.score).first();
+        this.animate($score, 'wiggle', 1000);
     },
     onClick: function(e) {
         e.preventDefault();
@@ -210,6 +273,7 @@ Board.prototype = {
         else if (this.currentShape !== null && shape.id == this.currentShape.id) {
             this.addShapeWithValidation(this.currentShape);
             this.currentShape = null;
+            this.defaultCursor();
         }
     },
     onEnter: function(e) {
@@ -251,7 +315,6 @@ Board.prototype = {
         this.clear();
         this.setColors();
         this.finished = false;
-        this.calculateScore();
     },
     onReduceSize: function() {
         if (this.size > this.minSize) {
@@ -311,6 +374,12 @@ Board.prototype = {
             return self.getShape(cell) === null;
         });
     },
+    moveCursor: function() {
+        this.$board.css('cursor', 'move');
+    },
+    defaultCursor: function() {
+        this.$board.css('cursor', 'auto');
+    },
     setColors: function() {
         var self = this;
         shuffle(this.colors);
@@ -321,7 +390,7 @@ Board.prototype = {
         });
         this.patterns.forEach(function(pattern) {
             self.colors.forEach(function(color) {
-                self.shapeColors.push('color-' + color + '-' + pattern);
+                self.shapeColors.push('color-' + color + ' pattern-' + pattern);
             });
         });
     },
@@ -333,19 +402,34 @@ Board.prototype = {
     reuseColor: function(color) {
         this.shapeColors.splice(this.shapeColors.indexOf(color), 1);
         this.shapeColors.unshift(color);
+    },
+    sortNumber: function(a, b) {
+        return a - b;
     }
 };
 var Cell = function(point) {
     this.point = point;
     this.$el = null;
+    this.$text = null;
     this.$remove = null;
     this.createElement();
 }
 
 Cell.prototype = {
     createElement: function() {
+        this.$text = $('<span class="text"></span>');
         this.$el = $('<div class="cell ' + this.point.id + '"></div>');
         this.$el.data('cell', this);
+        this.$el.append(this.$text);
+    },
+    text: function(text, position) {
+        this.$text.html(text).attr('style', '');
+        if (typeof position !== 'undefined') {
+            this.$text.css(position);
+        }
+    },
+    emptyText: function() {
+        this.text('');
     },
     enableRemove: function() {
         this.$remove = $('<button ' +
@@ -355,21 +439,25 @@ Cell.prototype = {
         this.$el.append(this.$remove);
     },
     disableRemove: function() {
-        this.$el.empty();
+        this.$remove.remove();
         this.$remove = null;
     },
     fill: function(color) {
         this.removeFill();
-        this.$el.addClass(color);
+        this.$el.addClass('fill').addClass(color);
     },
     removeFill: function() {
         this.$el.removeClass(function(index, css) {
-            return (css.match(/(^|\s)color-\S+/g) || []).join(' ');
-        });
+            return (css.match(/(^|\s)(color-|pattern-)\S+/g) || []).join(' ');
+        }).removeClass('fill');
+    },
+    empty: function() {
+        this.removeFill();
+        this.emptyText();
     }
 };
 
-var Corners = function(start, end) {
+var CornerPoints = function(start, end) {
     this.bottomLeft = new Point(start.x, start.y);
     this.topRight = new Point(end.x, end.y);
 
@@ -419,37 +507,41 @@ Point.prototype = {
 var Shape = function(start, end, color) {
     this.start = start;
     this.end = end;
+    this.width = null;
+    this.height = null;
     this.id = null;
+    this.cornerPoints = null;
     this.color = color;
     this.cells = [];
     this.value = 0;
+    this.cellWidth = 50;
 
     this.setId();
-    this.setCorners();
+    this.setCornerPoints();
 }
 
 Shape.prototype = {
     setId: function() {
-        var w = Math.abs(this.start.x - this.end.x) + 1;
-        var h = Math.abs(this.start.y - this.end.y) + 1;
-        if (w < h) {
-            this.id = w + 'x' + h;
+        this.width = Math.abs(this.start.x - this.end.x) + 1;
+        this.height = Math.abs(this.start.y - this.end.y) + 1;
+        if (this.width < this.height) {
+            this.id = this.width + 'x' + this.height;
         } else {
-            this.id = h + 'x' + w;
+            this.id = this.height + 'x' + this.width;
         }
-        this.value = w * h;
+        this.value = this.width * this.height;
     },
     setEnd: function(end) {
         this.end = end;
         this.setId();
-        this.setCorners();
+        this.setCornerPoints();
     },
-    setCorners: function() {
-        this.corners = new Corners(this.start, this.end);
+    setCornerPoints: function() {
+        this.cornerPoints = new CornerPoints(this.start, this.end);
     },
     has: function(cell) {
-        if (cell.point.x >= this.corners.bottomLeft.x && cell.point.x <= this.corners.topRight.x
-            && cell.point.y >= this.corners.bottomLeft.y && cell.point.y <= this.corners.topRight.y) {
+        if (cell.point.x >= this.cornerPoints.bottomLeft.x && cell.point.x <= this.cornerPoints.topRight.x
+            && cell.point.y >= this.cornerPoints.bottomLeft.y && cell.point.y <= this.cornerPoints.topRight.y) {
             return true;
         }
 
@@ -462,21 +554,69 @@ Shape.prototype = {
         this.cells[cell.point.id] = cell;
     },
     enableRemove: function() {
-        this.cells[this.corners.topRight.id].enableRemove();
+        this.topRight().enableRemove();
     },
     disableRemove: function() {
-        this.cells[this.corners.topRight.id].disableRemove();
+        this.topRight().disableRemove();
     },
     fill: function() {
         for (var id in this.cells) {
             var cell = this.cells[id];
             cell.fill(this.color);
         }
+        this.showSize();
+    },
+    showSize: function() {
+        this.startCell().text(this.value + '<br>' + this.id, this.sizePosition());
+    },
+    sizePosition: function() {
+        var offsetX = (this.cellWidth * (this.width / 2)) - (this.cellWidth / 2);
+        var offsetY = (this.cellWidth * (this.height / 2)) - (this.cellWidth / 2);
+
+        if (typeof this.topLeft() !== 'undefined' && this.start.id == this.topLeft().point.id) {
+            return {
+                'top': offsetY,
+                'left': offsetX
+            }
+        }
+        if (typeof this.topRight() !== 'undefined' && this.start.id == this.topRight().point.id) {
+            return {
+                'top': offsetY,
+                'right': offsetX
+            }
+        }
+        if (typeof this.bottomLeft() !== 'undefined' && this.start.id == this.bottomLeft().point.id) {
+            return {
+                'bottom': offsetY,
+                'left': offsetX
+            }
+        }
+        if (typeof this.bottomRight() !== 'undefined' && this.start.id == this.bottomRight().point.id) {
+            return {
+                'bottom': offsetY,
+                'right': offsetX
+            }
+        }
     },
     removeFill: function() {
         for (var id in this.cells) {
             var cell = this.cells[id];
-            cell.removeFill();
+            cell.empty();
         }
+    },
+    bottomLeft: function() {
+        return this.cells[this.cornerPoints.bottomLeft.id];
+    },
+    topRight: function() {
+        return this.cells[this.cornerPoints.topRight.id];
+    },
+    bottomRight: function() {
+        return this.cells[this.cornerPoints.bottomRight.id];
+    },
+    topLeft: function() {
+        return this.cells[this.cornerPoints.topLeft.id];
+    },
+    startCell: function() {
+        return this.cells[this.start.id];
     }
 };
